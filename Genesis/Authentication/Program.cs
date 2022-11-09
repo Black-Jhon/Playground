eb902d7b99f9635404a817cd172aeece34705d5c
john <chukwuchidieberejohn@gmail.com>
2022-11-09 17:24:57 +0100

    Updated Authentication.csproj, Program.cs, and 3 more files...

---
+++ Genesis/Authentication/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Authentication
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Register
+            Console.Write("Enter name to register: ");
+            var name = Console.ReadLine();
+            Console.Write("Enter password to register: ");
+            var password = Console.ReadLine();
+            
+            // Login
+            Console.Write("Enter name to login: ");
+            var firstName = Console.ReadLine();
+            Console.Write("Enter password to login: ");
+            var confirmPassword = Console.ReadLine();
+
+            if (name == firstName && password == confirmPassword)
+            {
+                Console.WriteLine($"You're logged in.");
+            }
+            else
+            {
+                Console.WriteLine($"Wrong credentials.");
+            }
+        }
+    }
+}
