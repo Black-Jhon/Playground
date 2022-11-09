647ad9bd3bcee3e209acdcae887bf78a8c77eb92
john <chukwuchidieberejohn@gmail.com>
2022-11-09 16:37:44 +0100

    Updated Program.cs, TransformString.exe, and 3 more files...

--- Genesis/TransformString/Program.cs
+++ Genesis/TransformString/Program.cs
@@ -6,6 +6,7 @@ namespace TransformString
     {
         public static void Main(string[] args)
         {
+            Console.Write("Please enter a value: ");
             var input = Console.ReadLine();
             var length = input.Length;
             var result = new String('#', length - 4) + input.Substring(length - 4);
