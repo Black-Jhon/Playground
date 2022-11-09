d3e88c4108384e0e460c01cac2d405c4d28297d6
john <chukwuchidieberejohn@gmail.com>
2022-11-09 17:03:13 +0100

    Updated Genesis.sln, Library.csproj, and 11 more files...

---
+++ Genesis/Library/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Library
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Choose between c, o, d, e, as your entry values: ");
+            var user = Console.ReadLine();
+            if (user == "c")
+            {
+                Console.WriteLine("C# Books");
+            }else if (user == "o")
+            {
+                Console.WriteLine(".NET Core Books");
+            }else if (user == "d")
+            {
+                Console.WriteLine("Clean Coding Books");
+            }else if (user == "e")
+            {
+                Console.WriteLine("EFCore Books");
+            }
+            else
+            {
+                Console.WriteLine("Sorry! This is an Invalid Entry. Check and try again!");
+            }
+        }
+    }
+}
