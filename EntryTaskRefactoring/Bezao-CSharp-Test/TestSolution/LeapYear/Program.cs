6eeaeb0061255390589a8e4cbb401100218e9016
john <chukwuchidieberejohn@gmail.com>
2022-11-15 08:19:10 +0100

    Updated indexLayout.xml, projectSettingsUpdater.xml, and 113 more files...

---
+++ EntryTaskRefactoring/Bezao-CSharp-Test/TestSolution/LeapYear/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace TestSolution;
+
+public class LeapYear
+{
+    public static void Main()
+    {
+        var newLeapYear = GetNextLeapDate(DateTime.Now);
+        Console.WriteLine(newLeapYear);
+
+        DateTime GetNextLeapDate(DateTime baseDate)
+        {
+            var year = baseDate.Year;
+
+            // Starts in the next year assuming we’re already in March
+            if (baseDate.Month > 2)
+                year++;
+
+            // Find the next leap year
+            while (!DateTime.IsLeapYear(year))
+                year++;
+
+            // Gets last of February
+            return new DateTime(year, 2, 29);
+        }
+    }
+}
---
+++ EntryTaskRefactoring/Bezao-CSharp-Test/TestSolution/PigLatin/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace TestSolution;
+
+// Base method
+public class PigLatin
+{
+    public static void Main()
+    {
+        var pigLatin = ToPigLatin("The quick brown fox");
+        Console.WriteLine(pigLatin);
+        Console.WriteLine(ToEnglish(pigLatin));
+    }
+
+    private static string ToPigLatin(string sentence)
+    {
+        var words = sentence.ToLower().Split(" ");
+        var result = new List<string>();
+        foreach (var word in words)
+        {
+            var newWord = word.Substring(1, word.Length - 1) + word[0] + "ay";
+            result.Add(newWord);
+        }
+
+        return string.Join(" ", result);
+    }
+
+    private static string ToEnglish(string sentence)
+    {
+        var words = sentence.ToLower().Split(" ");
+        var result = new List<string>();
+        foreach (var word in words)
+        {
+            var newWord = word[word.Length - 3] + word.Substring(0, word.Length - 3);
+            result.Add(newWord);
+        }
+
+        return string.Join(" ", result);
+    }
+}
