1f049e185f1fe653ed7646405d38d0ff8f164312
john <chukwuchidieberejohn@gmail.com>
2022-11-22 10:14:20 +0100

    initial commit

---
+++ MyATMApplication/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace MyATM
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Title = "My ATM APP";
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("\n------------------------------------Hello there! Good day and welcome to my ATM App------------------------------------");
+            Console.ReadLine();
+
+        }
+    }
+}
